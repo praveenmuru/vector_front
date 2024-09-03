b5b945b8d09ddcdfefe3d9df328c509e6d980546
adshay-cloudq <adshay@cloudq.net>
2024-09-03 06:19:07 +0000

    Sidebar.js updated

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Sidebar() {
+    const [isSidebarVisible, setSidebarVisible] = useState(false);
+
+    const toggleSidebar = () => {
+        setSidebarVisible(!isSidebarVisible);
+    };
+
     return (
-        <div className="sidebar">
-            <ul>
-                <li><Link to="/dashboard">Dashboard</Link></li>
-                <li><Link to="/profile">Profile</Link></li>
-                <li><Link to="/settings">Settings</Link></li>
-                <li><Link to="/logout">Logout</Link></li>
-            </ul>
+        <div>
+            <button className="hamburger-menu" onClick={toggleSidebar}>
+                ☰
+            </button>
+            {isSidebarVisible && (
+                <div className="sidebar">
+                    <ul>
+                        <li><Link to="Dashboard.js">Dashboard</Link></li>
+                        <li><Link to="/profile">Profile</Link></li>
+                        <li><Link to="/settings">Settings</Link></li>
+                        <li><Link to="/logout">Logout</Link></li>
+                    </ul>
+                </div>
+            )}
         </div>
     );
 }
