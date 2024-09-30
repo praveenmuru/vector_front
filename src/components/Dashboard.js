0b2485c5131a5c403d01d09a6b01ae18089f224f
adshay-cloudq <adshay@cloudq.net>
2024-09-30 09:00:22 +0000

    Tasks overview added in Dashboard page and styles are updated

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -20,11 +20,17 @@ function Dashboard() {
   const completedProjects = 10;
   const overdueProjects = 2;
   const upcomingProjects = 3;
+
+  const pendingTasks = 7;
+  const inProgressTasks = 8;
+  const completedTasks = 5;
+
   return (
     <div className="dashboard-container">
       <div className="dashboard-content">
         <h1>Welcome to the Dashboard, {name}!</h1>
-        <div className="metrics-grid">
+
+        <div className="project-status-grid">
           <div className="metric">
             <h3>Number of Active Projects</h3>
             <h1>{activeProjects}</h1>
@@ -46,6 +52,22 @@ function Dashboard() {
         <div className="status-pie-chart">
           <PieChartComponent />
         </div>
+        <br />
+        <div className="task-status-grid">
+          <div className="metric">
+            <h3>Number of Pending Tasks</h3>
+            <h1>{pendingTasks}</h1>
+          </div>
+          <div className="metric">
+            <h3>Number of Tasks in Progress</h3>
+            <h1>{inProgressTasks}</h1>
+          </div>
+          <div className="metric">
+            <h3>Number of Completed Tasks</h3>
+            <h1>{completedTasks}</h1>
+          </div>
+        </div>
+
       </div>
     </div>
   );
